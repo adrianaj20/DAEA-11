421c7f2911def1d25f6bc50461223e76115cf2f1
adrianaj20 <adrian.najarro@tecsup.edu.pe>
2023-11-13 20:19:18 -0500

    first

---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DocumentNumber { get; set; }
+    }
+}
